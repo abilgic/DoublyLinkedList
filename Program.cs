55813e74ab42fe3651ba7d9570023bfda0f8fc48
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-07-11 19:46:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using DoublyLinkedList;
+
+DLList dLList = new DLList();
+dLList.insert(1);
+dLList.insert(2);
+dLList.insert(3);
+dLList.insert(4);
+dLList.insert(5);
+dLList.Print();
+Console.WriteLine();
